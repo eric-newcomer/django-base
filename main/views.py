ab92c9dd6ec95f6b99d8847f928fe7790f6322bc
Eric Newcomer <ericnewcomer@microvu.com>
2018-08-16 14:44:21 -0700

    Add login page and login_required

--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import logout, login
 from django.contrib import messages
 from .forms import SignUpForm, UserForm
 from .backend import authenticate
+from django.views import View
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def signup(request):
@@ -22,9 +24,35 @@ def signup(request):
         'signup_form': signup_form,
     })
 
+class LoginView(View):
+
+    def get(self, request, *args, **kwags):
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'main/login.html')
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(
+                request, 'Failed to sign in! Please check your name and PIN', extra_tags='danger')
+            return redirect('/login')
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('/login')
+
+@login_required
 def index(request):
     return render(request, 'main/home.html')
 
-
+@login_required
 def about(request):
     return render(request, 'main/about.html')
